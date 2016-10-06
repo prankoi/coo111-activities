206d29ad4f4e8dcf5ad191ccf6317560b6d056c6
prankoi <batuken.manao@gmail.com>
2016-10-06 13:40:55 +0800

    Add files via upload

---
+++ MidtermModule/MidtermModule/DisplayAssign.vb
@@ -0,0 +1,23 @@
+﻿Module DisplayAssign
+
+    Sub OutputName(ByVal phoneName As Object, ByVal input As String)
+        phoneName.Text += input
+    End Sub
+
+    Sub OutputDesc(ByVal phoneDesc As Object, ByVal input As String)
+        phoneDesc.Text += input
+    End Sub
+
+    Sub OutputNull(ByVal var As Object)
+        var.Text = ""
+    End Sub
+
+    Sub AddDict(ByVal dict As Dictionary(Of String, String), ByVal key As String, ByVal value As String)
+        dict.Add(key, value)
+    End Sub
+
+    Sub AddItem(ByVal list As ListBox, ByVal item As String)
+        list.Items.Add(item)
+    End Sub
+
+End Module
